c79fa678cbd1f587a6519657b4229ab478b40881
Feri Agus Irawan <30733353+feriagusirawan@users.noreply.github.com>
2023-12-25 14:18:50 +0700

    Update koneksi.php
    Tipo

--- web-api/koneksi.php
+++ web-api/koneksi.php
@@ -1,6 +1,6 @@
 <?php 
 	include 'config.php';
 	
-	$konek = new Mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(Mysqli_errno());
+	$konek = new Mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(Mysqli_error());
 
  ?>
